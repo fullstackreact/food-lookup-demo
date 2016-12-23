c4a925be64320bab9537f36a0a8f24415a958eb3
Mark Erikson <mark@isquaredsoftware.com>
2016-12-22 22:15:14 -0500

    Fix logic error and formatting
    Fixed error in filter logic (need second callback arg to be index)
    Fixed formatting to 2-spaces for consistency

--- client/src/App.js
+++ client/src/App.js
@@ -3,37 +3,37 @@ import SelectedFoods from './SelectedFoods';
 import FoodSearch from './FoodSearch';
 
 class App extends Component {
-    state = {
-        selectedFoods : []
-    }
+  state = {
+      selectedFoods : []
+  }
     
-    removeFoodItem = (itemIndex) => {
-        const filteredFoods = this.state.selectedFoods.filter(idx => itemIndex !== idx);
-        this.setState({selectedFoods : filteredFoods});
-    }
+  removeFoodItem = (itemIndex) => {
+      const filteredFoods = this.state.selectedFoods.filter( (item, idx) => itemIndex !== idx);
+      this.setState({selectedFoods : filteredFoods});
+  }
     
-    addFood = (food) => {
-        const newFoods = this.state.selectedFoods.concat(food);
-        this.setState({selectedFoods : newFoods});
-    }
+  addFood = (food) => {
+      const newFoods = this.state.selectedFoods.concat(food);
+      this.setState({selectedFoods : newFoods});
+  }
     
-    render() {
-        const {selectedFoods} = this.state;
+  render() {
+    const {selectedFoods} = this.state;
         
-        return (
-          <div className='App'>
-			      <div className='ui text container'>
-			        <SelectedFoods
-				        foods={selectedFoods}
-				        onFoodClick={this.removeFoodItem}
-			        />
-			        <FoodSearch
-                onFoodClick={this.addFood}
-              />
-            </div>
-          </div>
-        );
-    }
+    return (
+      <div className='App'>
+        <div className='ui text container'>
+	        <SelectedFoods
+	          foods={selectedFoods}
+            onFoodClick={this.removeFoodItem}
+          />
+          <FoodSearch
+            onFoodClick={this.addFood}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
