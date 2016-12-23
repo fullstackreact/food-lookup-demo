5dae904a68a793110b4a272524f493a1c73bec02
Mark Erikson <mark@isquaredsoftware.com>
2016-12-22 22:02:16 -0500

    Rewrite App.js to use ES6 syntax
    Fixes #15
    
    Improved readability and style of App.js using ES6 syntax:
    
    Used React class component syntax instead of createClass
    Extracted callbacks as class methods instead of inline in JSX
    Used Class Properties syntax to define callback methods and initial state
    Used Array.filter to remove item instead of "double-slice" approach

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SelectedFoods from './SelectedFoods';
 import FoodSearch from './FoodSearch';
 
-const App = React.createClass({
-  getInitialState: function () {
-    return {
-      selectedFoods: [],
-    };
-  },
-  render: function () {
-    return (
-      <div className='App'>
-        <div className='ui text container'>
-          <SelectedFoods
-            foods={this.state.selectedFoods}
-            onFoodClick={
-              (idx) => (
-                this.setState({
-                  selectedFoods: [
-                    ...this.state.selectedFoods.slice(0, idx),
-                    ...this.state.selectedFoods.slice(
-                      idx + 1, this.state.selectedFoods.length
-                    ),
-                  ],
-                })
-              )
-            }
-          />
-          <FoodSearch
-            onFoodClick={
-              (food) => (
-                this.setState({
-                  selectedFoods: this.state.selectedFoods.concat(food),
-                })
-              )
-            }
-          />
-        </div>
-      </div>
-    );
-  },
-});
+class App extends Component {
+    state = {
+        selectedFoods : []
+    }
+    
+    removeFoodItem = (itemIndex) => {
+        const filteredFoods = this.state.selectedFoods.filter(idx => itemIndex !== idx);
+        this.setState({selectedFoods : filteredFoods});
+    }
+    
+    addFood = (food) => {
+        const newFoods = this.state.selectedFoods.concat(food);
+        this.setState({selectedFoods : newFoods});
+    }
+    
+    render() {
+        const {selectedFoods} = this.state;
+        
+        return (
+          <div className='App'>
+			      <div className='ui text container'>
+			        <SelectedFoods
+				        foods={selectedFoods}
+				        onFoodClick={this.removeFoodItem}
+			        />
+			        <FoodSearch
+                onFoodClick={this.addFood}
+              />
+            </div>
+          </div>
+        );
+    }
+}
 
 export default App;
